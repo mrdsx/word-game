4ada17b11b59d14a00ceae5dc6bad3821db566bf
mrdsx <andre0.castlegames@gmail.com>
2026-03-05 19:29:53 +0300

    chore: update frontend URL in prod and prod dependencies

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const FRONTEND_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000"
-    : "https://classic-word-game.netlify.app";
+    : "https://classic-word-game.vercel.app";
 
 export const MIN_WORD_LENGTH = 2;
 export const MAX_WORD_LENGTH = 30;
