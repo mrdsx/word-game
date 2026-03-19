d1b93708b561036d79bbb111850762367978f503
mrdsx <andre0.castlegames@gmail.com>
2026-03-19 17:54:42 +0300

    feat: add root endpoint

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -15,11 +16,19 @@ def create_app() -> FastAPI:
         CORSMiddleware,
         allow_origins=allowed_origins,
         allow_credentials=True,
-        allow_methods=["DELETE"],
+        allow_methods=["GET", "DELETE"],
         allow_headers=["Authorization", "Access-Control-Allow-Origin"],
     )
 
     _app.include_router(api_router, prefix="/api/v1")
+
+    @_app.get("/")
+    async def root():
+        return {
+            "api_version": "1",
+            "status": "ok",
+            "timestamp": datetime.now(),
+        }
 
     return _app
 
